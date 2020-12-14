3cf5c3f4581bfc95c1c7508d0c25c8a2963988a2
Francis Pegenia <42527972+FrancisRP@users.noreply.github.com>
2020-12-14 19:15:32 +0800

    done with the date picker

--- src/Banner.js
+++ src/Banner.js
@@ -10,7 +10,7 @@ function Banner() {
         <div className="banner">
             <div className="banner__search">
                 {showSearch && <Search />}
-                <Button onClick={() => setShowSearch(!showSearch)} className="banner__searchButton" variant="outlined">Search Dates</Button>
+                <Button onClick={() => setShowSearch(!showSearch)} className="banner__searchButton" variant="outlined"> {showSearch ? "Hide" : "Search Dates"}</Button>
             </div>
             <div className="banner__info">
                 <h1>Get out and stretch your imagination</h1>
