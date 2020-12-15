ee53f1ecf3798cfbe1bb0d1f44cfc559f637284a
Francis Pegenia <42527972+FrancisRP@users.noreply.github.com>
2020-12-15 15:40:24 +0800

    Done cloning AirBnB

--- src/Banner.js
+++ src/Banner.js
@@ -2,9 +2,10 @@ import React, {useState} from 'react'
 import "./banner.css";
 import { Button } from "@material-ui/core"
 import Search from './Search'
+import { useHistory } from 'react-router-dom';
 
 function Banner() {
-
+    const history = useHistory();
     const [showSearch, setShowSearch] = useState(false);
     return (
         <div className="banner">
@@ -15,7 +16,7 @@ function Banner() {
             <div className="banner__info">
                 <h1>Get out and stretch your imagination</h1>
                 <h5>Plan a different kind of getaway to uncover the hidden gems near you.</h5>
-                <Button variant="outlined"> Explore Nearby</Button>
+                <Button variant="outlined" onClick={() => history.push('/search')}> Explore Nearby</Button>
             </div>
         </div>
     )
